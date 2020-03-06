e64b7b6088add9078a2b2ff16069a38e365aa8b2
Chris Leech <chrisleech94@gmail.com>
2020-03-06 15:24:46 +0000

    Fixed code to export CSVs

--- t.py
+++ t.py
@@ -1,57 +1,45 @@
-#!/usr/bin/env python3
-
 import csv
 from time import sleep
 from datetime import datetime
 
-from api_token import token
+from api_token import token # Store API token in app_token.py
 
 import requests
 from tqdm import trange
+
+api_url = 'https://slack.com/api/'
 
 
 def write_dicts_to_csv(filename, dictionaries):
     field_names = dictionaries[0].keys()
     with open(filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=field_names,
-                                quoting=csv.QUOTE_NONNUMERIC)
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
         for dictionary in dictionaries:
             writer.writerow(dictionary)
 
 
-def get_access_logs():
-    all_access_logs = []
-    url = 'https://slack.com/api/team.billableInfo'
-    params = {'token': token, 'count': 1000}  # 1000 logs per page is maximum
 
-    print(' Downloading pages …')
-    for page in trange(1, 101):  # 100 pages is maximum
-        params['page'] = page
-        res = requests.get(url, params=params)
-        res_data = res.json()
+def get_users():
+    concat_url = api_url + 'users.list' # Create API URL with endpoint
+    params = {'token': token}
+    print(' Downloading users ...')
+    res = requests.get(concat_url, params=params)
+    res_data = res.json()
 
-        if not res_data['ok']:
-            raise ValueError(f'Something went wrong.'
-                             'URL: {res.url}'
-                             'Error: {res_data["error"]}')
+    if not res_data['ok']:
+        raise ValueError(f'Something went wrong.'
+                            'URL: {res.url}'
+                            'Error: {res_data["error"]}')
 
-        all_access_logs.extend(res_data['billable_info'])
+    return res_data['members']
 
-        sleep(3)  # Limit for Tier 2 is 20 req/min
-
-    return all_access_logs
-
+    sleep(3)  # Limit for Tier 2 is 20 req/min
 
 
 def main():
-    access_logs = get_access_logs()
-    write_dicts_to_csv('raw_data1.csv', access_logs)
-    #!last_logins = get_last_logins(access_logs)
-    #!members = get_all_members()
-    #!add_missing_members(last_logins, members)
-    #1write_dicts_to_csv('last_logins.csv', last_logins)
-
+    user_list = get_users()
+    write_dicts_to_csv('list_users.csv', user_list)
 
 if __name__ == '__main__':
    main()
